2bb03bc90b40bb547424ca60a739877183efa742
AntonioVdlC <antonio.villagra.de.la.cruz@gmail.com>
2015-10-26 00:47:44 -0400

    Adding the module.

---
+++ dist/index.js
@@ -0,0 +1,27 @@
+// List of the characters we want to escape and their HTML escaped version
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+var chars = {
+	"&": "&amp;",
+	">": "&gt;",
+	"<": "&lt;",
+	'"': "&quot;",
+	"'": "&#39;",
+	"`": "&#96;"
+};
+
+// Dynamically create a RegExp from the `chars` object
+var re = new RegExp(Object.keys(chars).join("|"), "g");
+
+// Return the escaped string
+
+exports["default"] = function (str) {
+	return String(str).replace(re, function (match) {
+		return chars[match];
+	});
+};
+
+module.exports = exports["default"];
